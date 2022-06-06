765e819780b111c97fbd52e9adbf8d8af3c0e44b
diya679 <97897457+diya679@users.noreply.github.com>
2022-06-06 19:38:50 +0530

    Add files via upload

---
+++ chat_room.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAjJwxJHj2z9c0xsbw8srf8QTAMqkGQ-is",
+    authDomain: "letschat-web-app-28394.firebaseapp.com",
+    databaseURL: "https://letschat-web-app-28394-default-rtdb.firebaseio.com",
+    projectId: "letschat-web-app-28394",
+    storageBucket: "letschat-web-app-28394.appspot.com",
+    messagingSenderId: "276043811854",
+    appId: "1:276043811854:web:2cc7f5066d337a3a9a805f"
+  };
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
